dc8c87346a658e3019479eda5780ff809c3bf945
Ridha Fahmi J <61994966+EvolutionRF@users.noreply.github.com>
2024-09-03 03:44:25 +0800

    fixing deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/static';
 
 // import vercel from '@astrojs/vercel';
 
